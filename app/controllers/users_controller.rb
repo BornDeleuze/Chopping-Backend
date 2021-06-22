42884003710e1d11928d5c48900259135b39aafd
Matt Beaudoin <ehxtwo@gmail.com>
2021-06-22 12:13:36 -0700

    modify user strong param code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,14 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
+    
     @user = User.new(user_params)
-
+    
     if @user.save
+      
       render json: @user, status: :created, location: @user
     else
+      
       render json: @user.errors, status: :unprocessable_entity
     end
   end
@@ -46,6 +49,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:name)
     end
 end
